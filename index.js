c11c5d853f0a9e82c38f18068affdf807d4cd1cf
ShahmeerRabbani <126972498+ShahmeerRabbani@users.noreply.github.com>
2025-10-19 06:43:49 +0500

    done

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ connectDB()
 const app = express()
 const PORT = 5000 || process.env.PORT
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://final-hackathon-one-beta.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true, // if you’re using cookies or tokens
+  })
+);
+
+// ✅ Handle preflight requests explicitly
+app.options('*', cors());
 
 app.use("/", userRoutes)
 
